5137ee556fc4bf0a862f90f5ec1abd5c998941c0
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-05-09 14:03:40 +0700

    Clean

--- src/BeeAZZ/SlapperSound/Main.php
+++ src/BeeAZZ/SlapperSound/Main.php
@@ -10,31 +10,34 @@ use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use slapper\events\SlapperHitEvent;
 
 class Main extends PluginBase implements Listener{
+
+  proteted $cfg;
   
-  public function onEnable():void{
+  protected function onEnable():void{
     $this->saveDefaultConfig();
+    $this->cfg = $this->getConfig();
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
   
   public function onHitSlapper(SlapperHitEvent $event){
    $player = $event->getDamager();
-  if($this->getConfig()->get("AddSound") == true){
-   if($player instanceof Player){
-    $sound = $this->getConfig()->get("sound");
-    $volume = $this->getConfig()->get("volume");
-    $pitch = $this->getConfig()->get("pitch");
-    $this->playSound($player, $sound, $volume, $pitch);
+   if($this->cfg->get("AddSound") === true){
+    if($player instanceof Player){
+     $sound = $this->cfg->get("sound");
+     $volume = $this->cfg->get("volume");
+     $pitch = $this->cfg->get("pitch");
+     $this->playSound($player, $sound, $volume, $pitch);
    }
    }
   }
-  public function playSound(Player $player, string $sound, float $volume = 0, float $pitch = 0):void {
-		$packet = new PlaySoundPacket();
-		$packet->soundName = $sound;
-		$packet->x = $player->getPosition()->getX();
-		$packet->y = $player->getPosition()->getY();
-		$packet->z = $player->getPosition()->getZ();
-		$packet->volume = $volume;
-		$packet->pitch = $pitch;
-		$player->getNetworkSession()->sendDataPacket($packet);
-}
+  protected function playSound($player, string $sound, float $volume = 0, float $pitch = 0): void{
+    $packet = new PlaySoundPacket();
+    $packet->soundName = $sound;
+    $packet->x = $player->getPosition()->getX();
+    $packet->y = $player->getPosition()->getY();
+    $packet->z = $player->getPosition()->getZ();
+    $packet->volume = $volume;
+    $packet->pitch = $pitch;
+    $player->getNetworkSession()->sendDataPacket($packet);
+  }
 }
